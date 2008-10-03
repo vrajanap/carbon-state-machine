e389be72d818aca2dc3e9598ec4b35708b96f426
vrajanap <vrajanap@6cd178ee-90f9-11dd-b633-d39c22b7b77f>
2008-10-03 05:53:43 +0000

    committing with errors. ;-)

--- carbon/carbon_op_echoserver.cc
+++ carbon/carbon_op_echoserver.cc
@@ -7,6 +7,7 @@ void
 carbon_op_echoserver::handleRequest(requestTypes::NetRequest * data, unsigned long dataSize)
 {
   cout << "EchoServer::Sending " << data->getBuffer() << " to client" << std::endl; 
+  waitReqCount->incrementCounter();
   out(data, dataSize);
   return;
 }
@@ -18,8 +19,33 @@ carbon_op_echoserver::geninRequest()
   request += "\r\n\r\n";
   char * request_str = new char[request.length()];
   strcpy(request_str, request.c_str());
+ 
   return new requestTypes::NetRequest(request_str,strlen(request_str),0);
 
+}
+
+void
+carbon_op_echoserver::my_install(void * data)
+{
+}
+
+void *
+carbon_op_echoserver::my_get()
+{
+  return NULL;
+}
+
+void
+carbon_op_echoserver::my_purge(void * data)
+{
+}
+
+void
+carbon_op_echoserver::my_init()
+{
+	waitReqCount = 
+	request_counter::RequestCount_p(  	
+		new request_counter::RequestCount(getNewMutex()));
 }
 
 
