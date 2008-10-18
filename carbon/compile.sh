2be77a39eebee008ed694e7ed655f953e7f48cbb
vrajanap <vrajanap@6cd178ee-90f9-11dd-b633-d39c22b7b77f>
2008-10-18 05:42:02 +0000

    Minor changes for CS machines

--- carbon/compile.sh
+++ carbon/compile.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 if [[ ! -h ./sysmodel ]]
-then echo "Creating sysmodel symlink..." && ln -s /usr/local/include/sysmodel ./sysmodel
+then echo "Creating symlink..." && ln -s /projects/lasr/space5/lagniappe/include/sysmodel ./sysmodel
 fi
 if [[ ! -h ./jar ]]
-then echo "Creating jar symlink..." && ln -s /usr/local/jar ./jar
+then echo "Creating jar symlink..." && ln -s /projects/lasr/space5/lagniappe/jar ./jar
 fi
 echo "Generating new application files..."
-java -jar ./jar/lc.jar --app $1.xml
+java -jar /projects/lasr/space5/lagniappe/jar/lc.jar --app $1.xml
 echo "Generating new main file..."
 java -jar ./jar/lc.jar --main $1.xml sysmodel/cscamera.xml
 #echo "Generating profile files..."
